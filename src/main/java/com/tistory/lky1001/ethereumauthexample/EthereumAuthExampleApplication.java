3a34522999102a14e69969e1694f50c8312e4573
lky1001 <lky1001@gmail.com>
2018-06-14 22:58:22 +0900

    init spring boot project

---
+++ src/main/java/com/tistory/lky1001/ethereumauthexample/EthereumAuthExampleApplication.java
@@ -0,0 +1,12 @@
+package com.tistory.lky1001.ethereumauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumAuthExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EthereumAuthExampleApplication.class, args);
+    }
+}
